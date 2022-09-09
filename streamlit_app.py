af46aade6976801118145533af6525f4f5ce2414
andreiagmartins1810 <113178951+andreiagmartins1810@users.noreply.github.com>
2022-09-09 14:44:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import streamlit
 import pandas
 
 
-my_fruit_list = my_fruit_list.set_index
 
 streamlit.title('My Parents New Healty Diner')
 
@@ -15,3 +14,4 @@ streamlit.text('🥑🍞 Avocato Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
